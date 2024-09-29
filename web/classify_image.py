14d700b46ef7490d5b9d1ddeeb1d1eeed5f4d5cd
Chandan Mohan <33127046+imchandanmohan@users.noreply.github.com>
2024-09-29 15:40:10 -0400

    Update classify_image.py

--- web/classify_image.py
+++ web/classify_image.py
@@ -18,7 +18,7 @@ Run image classification with Inception trained on ImageNet 2012 Challenge data
 set.
 This program creates a graph from a saved GraphDef protocol buffer,
 and runs inference on an input JPEG image. It outputs human readable
-strings of the top 5 predictions along with their probabilities.
+strings of the top 5 predictions along with their probabilitie.
 Change the --image_file argument to any jpg image to compute a
 classification of that image.
 Please see the tutorial and website for a detailed description of how
